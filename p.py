03f0ee2faf34b99cb7fd1e44675176e5a96af06e
bala44858 <bala44858@gmail.com>
2022-11-01 18:57:51 +0530

    done

--- p.py
+++ p.py
@@ -1,1 +1,12 @@
-print("heool9jshc ")
+from re import L
+
+
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    ans=[]
+    for i in range(1,n):
+        if l[i]!=l[i-1]:
+            ans.append(i)
+            ans.append(i-1)
+    print(len(set(ans)))

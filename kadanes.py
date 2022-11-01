9d91f3bb266af93f8c94c73fa677e06be1c7d342
bala44858 <bala44858@gmail.com>
2022-11-01 14:43:15 +0530

    commit

---
+++ kadanes.py
@@ -0,0 +1,13 @@
+# kadanes algorithm for finding the maximum sum of sub array 
+
+
+n=int(input())
+l=list(map(int,input().split()))
+max_final=l[0]
+current_sum=0
+for i in l:
+    current_sum+=i
+    max_final=max(max_final,current_sum)
+    if current_sum<0:
+        current_sum=0
+print(max_final)
